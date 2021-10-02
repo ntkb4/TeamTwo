ed18c7919106315c3c90e813805df2d98354e5ef
Martha <Martha@10.0.0.245>
2021-10-02 13:49:39 -0400

    Martha

--- src/two/DemoTeamTwo.java
+++ src/two/DemoTeamTwo.java
@@ -3,5 +3,15 @@ package two;
 public class DemoTeamTwo {
 	public static void addNum(int a, int b, int c) {
 		System.out.println(a + b + c);
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		int a= 3;
+		int b= 2;
+		int c= 4;
+		addNum(a,b,c);
+		
 	}
 }
