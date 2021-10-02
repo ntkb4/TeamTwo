9e2464fa6d4265756ab4457d8960118e66b30245
aandagandg <aandagandg@mail.ru>
2021-10-02 13:48:00 -0400

    .

--- src/two/DemoTeamTwo.java
+++ src/two/DemoTeamTwo.java
@@ -4,4 +4,10 @@ public class DemoTeamTwo {
 	public static void addNum(int a, int b, int c) {
 		System.out.println(a + b + c);
 	}
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 2;
+		int c = 4;
+		addNum(a, b, c);
+	}
 }
