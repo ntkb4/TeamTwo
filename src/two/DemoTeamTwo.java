4a5e787c7ffe0dbdcbd319d139d97cbe8c323d81
tanvirrandhawa <tanvirrandhawa@tanvirs-mbp.fios-router.home>
2021-10-02 13:58:47 -0400

    commit from surinder

--- src/two/DemoTeamTwo.java
+++ src/two/DemoTeamTwo.java
@@ -3,6 +3,10 @@ package two;
 public class DemoTeamTwo {
 	public static void addNum(int a, int b, int c) {
 		System.out.println(a + b + c);
+	
+	}
+	public static void subNum(int a, int b, int c) {
+		System.out.println(a-b-c);
 	}
 	public static void main(String[] args) {
 		int a = 3;
