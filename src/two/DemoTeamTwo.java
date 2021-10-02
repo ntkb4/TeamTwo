783a4ac8e1374215477da3bbff5715ba705b013b
Martha <Martha@10.0.0.245>
2021-10-02 14:12:21 -0400

    Commit by Martha

--- src/two/DemoTeamTwo.java
+++ src/two/DemoTeamTwo.java
@@ -3,7 +3,7 @@ package two;
 public class DemoTeamTwo {
 	public static void addNum(int a, int b, int c) {
 		System.out.println(a + b + c);
-<<<<<<< HEAD
+
 		
 		
 	}
@@ -14,17 +14,14 @@ public class DemoTeamTwo {
 		int c= 4;
 		addNum(a,b,c);
 		
-=======
+
 	
 	}
 	public static void subNum(int a, int b, int c) {
 		System.out.println(a-b-c);
->>>>>>> branch 'master' of https://github.com/ntkb4/TeamTwo.git
+
 	}
-	public static void main(String[] args) {
-		int a = 3;
-		int b = 2;
-		int c = 4;
-		addNum(a, b, c);
+	
+	
+	
 	}
-}
